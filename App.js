a9dddb58a500e9b9628b941388ddb3d71f34e3e4
bharathpotla <bharathpotla@gmail.com>
2024-02-12 19:22:02 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,31 @@
+ 
+ const parent = React.createElement("div",{id: "parent"},
+ [
+    React.createElement("div",{id: "child"},
+    [React.createElement("h1",{},"I,m H1 Tag"),
+    React.createElement("h2",{},"I,m H2 Tag"),
+    ]),
+    React.createElement("div",{id: "child"},
+    [React.createElement("h1",{},"I,m H1 Tag"),
+    React.createElement("h2",{},"I,m H2 Tag"),
+ ])
+ ]);
+ 
+ /* const heading = React.createElement("h1", {id: "header"}, "Welcome To React world");
+ 
+ <script>
+
+   const heading = document.createElement("h1");
+   heading.innerHTML=("Hello World Welcome Javascript");
+
+   const root = document.getElementById("root");
+   root.appendChild(heading);
+
+</script>
+
+
+ */
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(parent);
